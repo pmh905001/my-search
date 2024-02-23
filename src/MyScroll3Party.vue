9e03f1ba1dda51799d8b1571aa0e657f21af7ed9
pmh905001 <pmh905001@126.com>
2024-02-23 15:31:21 +0800

    introduce third component v3-infinite-loading

---
+++ src/MyScroll3Party.vue
@@ -0,0 +1,89 @@
+<style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  padding: 20px;
+  margin: 20px auto 20px auto;
+  /* max-height: 400px; */
+  max-height: calc(100vh - 100px);
+  /* max-width: 400px; */
+  max-width: 100%;
+  /* overflow-y: scroll; */
+  overflow-y: auto;
+}
+
+.result {
+  font-weight: 300;
+  width: 80%;
+  padding: 20px;
+  text-align: center;
+  background: #eceef0;
+  border-radius: 10px;
+}
+</style>
+
+
+<template>
+  <div id="app">
+    <div class="result" v-for="comment in comments" :key="comment.id">
+      <div>{{ comment.email }}</div>
+      <div>{{ comment.id }}</div>
+    </div>
+    <infinite-loading target="#app" @infinite="load"></infinite-loading>
+  </div>
+</template>
+
+
+
+
+<script>
+import InfiniteLoading from "v3-infinite-loading"
+import "v3-infinite-loading/lib/style.css"
+
+export default {
+  components: {
+    // InfiniteLoading: infinite-loading,//bad
+    // InfiniteLoading,
+    "infinite-loading": InfiniteLoading,
+  },
+
+  data() {
+    return {
+      comments: [],
+      page: 1,
+    }
+  },
+
+  methods: {
+    async load($state) {
+      console.log('testing')
+      console.log("loading...");
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=10&_page=" + this.page);
+        const json = await response.json();
+        if (json.length < 10) {
+          $state.complete();
+        }
+        else {
+          this.comments.push(...json);
+          $state.loaded();
+        }
+        this.page++;
+      } catch (error) {
+        $state.error();
+      }
+    },
+
+  },
+
+}
+
+</script>
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -5,11 +5,24 @@ import App from './App.vue'
 // import MyFav from './MyFav.vue'
 // import MyScroll from './MyScroll.vue'
 // import MyScroll2 from './MyScroll2.vue'
-import MyScrollOpt from './MyScrollOpt.vue'
+// import MyScrollOpt from './MyScrollOpt.vue'
 
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
 // createApp(MyFav).mount('#app')
 // createApp(MyScroll).mount('#app')
 // createApp(MyScroll2).mount('#app')
 // createApp(MyScrollOpt).mount('#app')
+
+
+
+
+
+// import InfiniteLoading from "v3-infinite-loading"
+// import "v3-infinite-loading/lib/style.css"
+// import MyScroll3Party from './MyScroll3Party.vue'
+// const app = createApp(MyScroll3Party)
+// app.component("infinite-loading", InfiniteLoading);
+// app.mount("#app");
+import MyScroll3Party from './MyScroll3Party.vue'
+createApp(MyScroll3Party).mount('#app');
