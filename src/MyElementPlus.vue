f0b7ee858f0498083dfa62266b9c06a12a9a688a
pmh905001 <pmh905001@126.com>
2024-02-26 23:53:24 +0800

    test why input element with search icon not working on option vue

--- src/MyElementPlus.vue
+++ src/MyElementPlus.vue
@@ -1,27 +1,86 @@
-
-
-
 <template>
-  <el-input v-model="text" clearable style="width: 200px"  />
-
-
-
-  <el-button type="primary">leenhem</el-button>
-
-  <br/>
-
-  <el-button type="primary" :suffix-icon="['el-icon-search']">Search</el-button>
-
-
+  <div class="demo-input-size">
+    <el-input
+      v-model="input1"
+      class="w-50 m-2"
+      size="large"
+      placeholder="Please Input"
+    />
+    <el-input v-model="input2" class="w-50 m-2" placeholder="Please Input" />
+    <el-input
+      v-model="input3"
+      class="w-50 m-2"
+      size="small"
+      placeholder="Please Input"
+    />
+  </div>
+  <div class="demo-input-size">
+    <el-input
+      v-model="input1"
+      class="w-50 m-2"
+      size="large"
+      placeholder="Please Input"
+      :suffix-icon="Search"
+    />
+    <el-input
+      v-model="input2"
+      class="w-50 m-2"
+      placeholder="Please Input"
+      :suffix-icon="Search"
+    />
+    <el-input
+      v-model="input3"
+      class="w-50 m-2"
+      size="small"
+      placeholder="Please Input"
+      :suffix-icon="Search"
+    />
+  </div>
+  <div class="demo-input-size">
+    <el-input
+      v-model="input1"
+      class="w-50 m-2"
+      size="large"
+      placeholder="Please Input"
+      :prefix-icon="Search"
+    />
+    <el-input
+      v-model="input2"
+      class="w-50 m-2"
+      placeholder="Please Input"
+      :prefix-icon="Search"
+    />
+    <el-input
+      v-model="input3"
+      class="w-50 m-2"
+      size="small"
+      placeholder="Please Input"
+      :prefix-icon="Search"
+    />
+  </div>
 </template>
 
+<!-- <script lang="ts" setup>
+import { ref } from 'vue'
+import { Search } from '@element-plus/icons-vue'
+const input1 = ref('')
+const input2 = ref('')
+const input3 = ref('')
+</script> -->
+
 <script>
+  
+  
 export default {
+
+
   data() {
     return {
-      text: "test"
+      input1: '',
+      input2: '',
+      input3: '',
+
     }
   },
-}
+} 
 </script>
-
